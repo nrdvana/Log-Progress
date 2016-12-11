f565852d3ae2603538b8c6cba72d537a9f3f7153
Michael Conrad <mike@nrdvana.net>
2016-12-11 12:09:25 -0500

    Correctly handle partial lines at end of input
    Also support resuming the parse without re-reading entire file.

--- perl/lib/Log/Progress/Parser.pm
+++ perl/lib/Log/Progress/Parser.pm
@@ -2,27 +2,38 @@ package Log::Progress::Parser;
 use Moo 2;
 use JSON;
 
-has input  => ( is => 'rw' );
-has status => ( is => 'rw' );
+has input     => ( is => 'rw' );
+has input_pos => ( is => 'rw' );
+has status    => ( is => 'rw', default => sub { {} } );
 
 sub parse {
 	my $self= shift;
-	$self->status({});
 	my $fh= $self->input;
 	if (!ref $fh) {
 		my $input= $fh;
 		undef $fh;
 		open $fh, '<', \$input or die;
 	}
+	if ($self->input_pos) {
+		seek($fh, $self->input_pos, 0)
+			or die "seek: $!";
+	}
 	# TODO: If input is seekable, then seek to end and work backward
 	#  Substeps will make that rather complicated.
+	my $pos;
+	my %parent_cleanup;
 	while (<$fh>) {
+		last unless substr($_,-1) eq "\n";
+		$pos= tell($fh);
 		next unless $_ =~ /^progress: (([[:alpha:]][\w.]*) )?(.*)/;
 		my ($step_id, $remainder)= ($2, $3);
 		my $status= $self->status;
+		my @status_parent;
 		if (defined $step_id) {
-			$status= ($status->{step}{$_} //= { idx => scalar(keys %{$status->{step}}) - 1 })
-				for split /\./, $step_id;
+			for (split /\./, $step_id) {
+				push @status_parent, $status;
+				$status= ($status->{step}{$_} //= { idx => scalar(keys %{$status->{step}}) - 1 });
+			}
 		}
 		# First, check for progress number followed by optional message
 		if ($remainder =~ m,^([\d.]+)(/(\d+))?( (.*))?,) {
@@ -34,6 +45,11 @@ sub parse {
 				$status->{pos}= $num;
 				$status->{max}= $denom;
 				$status->{progress} /= $denom;
+			}
+			if ($status->{contribution}) {
+				# Need to apply progress to parent nodes at end
+				$parent_cleanup{$status_parent[$_]}= [ $_, $status_parent[$_] ]
+					for 0..$#status_parent;
 			}
 		}
 		elsif ($remainder =~ m,^\(([\d.]+)\) (.*),) {
@@ -49,17 +65,16 @@ sub parse {
 			warn "can't parse progress message \"$remainder\"\n";
 		}
 	}
-	$self->status->{progress} //= $self->_progress_from_substeps($self->status);
-}
-
-sub _progress_from_substeps {
-	my ($self, $status)= @_;
-	return unless $status->{step};
-	my $p= 0;
-	for (values %{$status->{step}}) {
-		$p += ($_->{progress} //= $self->_progress_from_substeps($_)) * $_->{contribution};
+	# Mark file position for next call
+	$self->input_pos($pos);
+	# apply child progress contributions to parent nodes
+	for (sort { $b->[0] <=> $a->[0] } values %parent_cleanup) {
+		my $status= $_->[1];
+		$status->{progress}= 0;
+		for (values %{$status->{step}}) {
+			$status->{progress} += $_->{progress} * $_->{contribution};
+		}
 	}
-	$p;
 }
 
 1;
--- perl/t/11-parse.t
+++ perl/t/11-parse.t
@@ -20,12 +20,12 @@ is_deeply(
 	'simple progress'
 ) or diag explain $parser->status;
 
-$parser= Log::Progress::Parser->new(input => <<'END');
-progress: foo (.5) Step 1
+$parser= Log::Progress::Parser->new(input => 
+"progress: foo (.5) Step 1
 progress: bar (.5) Step 2
 progress: foo 0/10
 progress: bar 1/10 - Status message
-END
+progress: bar 5/10" );  # Final line doesn't count because no newline
 
 $parser->parse;
 is_deeply(
@@ -44,6 +44,30 @@ is_deeply(
 			contribution => .5,
 			progress => .1, pos => 1, max => 10,
 			message => 'Status message',
+		},
+	}},
+	'substep progress',
+) or diag explain $parser->status;
+
+# Now, extend the input and parse more of it
+$parser->input($parser->input . " - New Status Message\n");
+$parser->parse;
+is_deeply(
+	$parser->status,
+	{ progress => .25, step => {
+		foo => {
+			idx => 0,
+			title => "Step 1",
+			contribution => .5,
+			progress => 0, pos => 0, max => 10,
+			message => undef,
+		},
+		bar => {
+			idx => 1,
+			title => "Step 2",
+			contribution => .5,
+			progress => .5, pos => 5, max => 10,
+			message => 'New Status Message',
 		},
 	}},
 	'substep progress',
